cb984c65e11eeba29445ecbfee5bbdc5d358233e
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-29 21:55:06 -0500

    m

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,17 +1,13 @@
-package conroller;
+package controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//We try to reach to main page
-@RestController
-public class HomeController {
-	
+@Controller
+class HomeController {
 	@RequestMapping("/")
 	public String home() {
-		
 		return "index.html";
-		
 	}
-	
 }
